afe400256e668e30ed723f9ff528a57a4632458b
Amy Woodworth <woodworthcommunications@gmail.com>
2016-04-19 10:25:52 -0500

    Update gallery.js to move photos from div to list. Adjust styles for list display. Remove path info from image and thumbnail in photos.js.

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,26 @@
+// Add search capability -- still needs work
 var defaultText = 'Search';
 var searchBox = document.getElementById('search');
 // Default text after load
 searchBox.value = defaultText;
 
-
+// Create list to display all photo thumbnails
 var photoDisplay =
-  document.createElement('div');
-  photoDisplay.className = 'main_gallery';
+  document.createElement('ul');
+  photoDisplay.className = 'photo_list';
 
-for (var i =0; i < photos.length; i++) {
+for (var i = 0; i < photos.length; i++) {
+  var photoList = document.createElement('li');
+  photoList.className = 'photo_list_item';
+
+// Add thumbnail images with name and caption for alt description
   var photoThumb = document.createElement('img');
+  photoThumb.src = 'Photos/Thumbnails/' + photos[i].thumbnail;
+  photoThumb.alt =  'Photo name: ' + photos[i].name +
+                    '. Photo description: ' + photos[i].caption;
   console.log(photoThumb);
-  photoThumb.src = photos[i].thumbnail;
-  photoDisplay.appendChild(photoThumb);
+  photoList.appendChild(photoThumb);
+  photoDisplay.appendChild(photoList);
 }
-document.body.appendChild(photoDisplay);
+// Add list to existing gallery div
+document.querySelector('.gallery').appendChild(photoDisplay);
