554e78059d3b8fe26c83c06761b4054dff723e6e
Amy Woodworth <woodworthcommunications@gmail.com>
2016-04-26 10:10:11 -0500

    Get larger photo to display when selecting the thumbnail image. Add border around photos and update class info in css.

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,8 @@
 // Add search capability -- still needs work
-var defaultText = 'Search';
+// var defaultText = 'Search';
 var searchBox = document.getElementById('search');
 // Default text after load
-searchBox.value = defaultText;
+// searchBox.value = defaultText;
 
 // Create list to display all photo thumbnails
 var photoDisplay =
@@ -15,11 +15,21 @@ for (var i = 0; i < photos.length; i++) {
 
 // Add thumbnail images with name and caption for alt description
   var photoThumb = document.createElement('img');
+  photoThumb.className = 'photo_thumbnail';
   photoThumb.src = 'Photos/Thumbnails/' + photos[i].thumbnail;
   photoThumb.alt =  'Photo name: ' + photos[i].name +
                     '. Photo description: ' + photos[i].caption;
-  console.log(photoThumb);
-  photoList.appendChild(photoThumb);
+  //console.log(photoThumb);
+
+// Add link to get fullsize image for lightbox display
+  var photoLink = document.createElement('a');
+  photoLink.className = 'view_photo';
+  photoLink.setAttribute('href', 'Photos/' + photos[i].image);
+  //console.log(photoLink);
+
+// Build photo gallery
+  photoLink.appendChild(photoThumb);
+  photoList.appendChild(photoLink);
   photoDisplay.appendChild(photoList);
 }
 // Add list to existing gallery div
